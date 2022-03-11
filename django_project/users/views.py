fb59fb0c85b1690b8be8a77cfc203bdfabab1edb
Jim <jim@jam.sessionsnet.org>
2022-03-11 13:26:49 -0800

    save the validated form in the database

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -9,6 +9,7 @@ def register(request):
         form = UserCreationForm(request.POST)
 
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('blog-home')
