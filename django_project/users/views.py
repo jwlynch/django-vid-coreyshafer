c02c4b0bafc635386b1d24949a67116d0b198147
Jim <jim@jam.sessionsnet.org>
2022-03-11 18:18:49 -0800

    add email addr to UserCreationForm with new class

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
+from .forms import UserRegisterForm
+
 def register(request):
     if request.method != 'POST': # form method is anything but post
-        form = UserCreationForm()
+        form = UserRegisterForm()
     else: # form method is post
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
 
         if form.is_valid():
             form.save()
